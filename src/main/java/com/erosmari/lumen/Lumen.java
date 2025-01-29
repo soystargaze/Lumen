ff7f94d34f839e285e20ab4623266709b91399ef
stargaze <mail@erosmari.com>
2025-01-29 01:23:03 +0100

    added languages

--- src/main/java/com/erosmari/lumen/Lumen.java
+++ src/main/java/com/erosmari/lumen/Lumen.java
@@ -112,7 +112,7 @@ public class Lumen extends JavaPlugin implements Listener {
             return;
         }
 
-        String[] defaultLanguages = {"en_us.yml", "es_es.yml", "fr_fr.yml", "de_de.yml", "it_it.yml", "pt_br.yml"};
+        String[] defaultLanguages = {"en_us.yml", "es_es.yml", "fr_fr.yml", "de_de.yml", "it_it.yml", "pt_br.yml", "es_andaluh.yml"};
 
         for (String languageFile : defaultLanguages) {
             saveDefaultTranslation(languageFile);
