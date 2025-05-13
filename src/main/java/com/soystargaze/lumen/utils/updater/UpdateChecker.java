4a6e4c5c9c9e3cd88eb36f77edd7beebceab989b
stargaze <mail@erosmari.com>
2025-05-13 17:13:02 +0200

    pre text handler

---
+++ src/main/java/com/soystargaze/lumen/utils/updater/UpdateChecker.java
@@ -0,0 +1,64 @@
+package com.soystargaze.lumen.utils.updater;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.soystargaze.lumen.Lumen;
+import com.soystargaze.lumen.utils.AsyncExecutor;
+import com.soystargaze.lumen.utils.text.TextHandler;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+
+public class UpdateChecker {
+
+    private static final String API_URL = "https://api.modrinth.com/v2/project/%s/version";
+    private static final String PROJECT_ID = "lumen";
+    @SuppressWarnings("deprecation")
+    private static final String CURRENT_VERSION = Lumen.getInstance().getDescription().getVersion();
+    private static final String DOWNLOAD_URL = "https://modrinth.com/plugin/lumen";
+
+    public static void checkForUpdates() {
+        AsyncExecutor.getExecutor().execute(() -> {
+            try {
+                String url = String.format(API_URL, PROJECT_ID);
+                URI uri = new URI(url);
+                URL urlObj = uri.toURL();
+                HttpURLConnection connection = (HttpURLConnection) urlObj.openConnection();
+                connection.setRequestMethod("GET");
+                connection.setRequestProperty("User-Agent", "soystargaze/Lumen/" + CURRENT_VERSION + " (dev@soystargaze.com)");
+
+                int responseCode = connection.getResponseCode();
+                if (responseCode == 200) {
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        response.append(line);
+                    }
+                    reader.close();
+
+                    Gson gson = new Gson();
+                    JsonArray versions = gson.fromJson(response.toString(), JsonArray.class);
+                    if (!versions.isEmpty()) {
+                        JsonObject latestVersion = versions.get(0).getAsJsonObject();
+                        String latestVersionNumber = latestVersion.get("version_number").getAsString();
+
+                        if (!CURRENT_VERSION.equals(latestVersionNumber)) {
+                            TextHandler.get().logTranslated("plugin.update_available", latestVersionNumber, DOWNLOAD_URL);
+                        } else {
+                            TextHandler.get().logTranslated("plugin.no_update_available");
+                        }
+                    }
+                } else {
+                    TextHandler.get().logTranslated("plugin.update_check_failed", responseCode);
+                }
+            } catch (Exception e) {
+                TextHandler.get().logTranslated("plugin.update_check_error", e.getMessage());
+            }
+        });
+    }
+}
---
+++ src/main/java/com/soystargaze/lumen/utils/updater/UpdateOnJoinListener.java
@@ -0,0 +1,22 @@
+package com.soystargaze.lumen.utils.updater;
+
+import com.soystargaze.vitamin.config.ConfigHandler;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class UpdateOnJoinListener implements Listener {
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        if (!ConfigHandler.getConfig().getBoolean("update-check", true)) {
+            return;
+        }
+        Player player = event.getPlayer();
+        if (player.isOp()) {
+            UpdateChecker.checkForUpdates();
+        }
+    }
+
+}
